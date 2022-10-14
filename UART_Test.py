574e1b079ed02ba4a1bb9f7cd09a226bf6c6f8e2
Carl-Olivier N'DIaye <carl-olivier.ndiaye@protonmail.com>
2022-10-14 13:22:46 +0200

    [FIX] Check sanity

--- UART_Test.py
+++ UART_Test.py
@@ -53,10 +53,11 @@ serial = Serial("/dev/ttyS0", baudrate=9600, stopbits=2, parity="odd")
 time.sleep(0.5)
 print("\nREAD BAUDRATE:9600, stopbits=2, parity=odd\n")
 for i in range (100):
-    buf = serial.read(1, 2)
+    buf = serial.read(16, 2)
     packet = ba.bitarray(endian='big')
     packet.frombytes(buf)
     print(sanity_check_packet(packet))
+serial.flush()
 serial.close()
 
 print("---------------------------")
