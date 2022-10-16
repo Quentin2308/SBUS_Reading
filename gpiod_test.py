a50daab124c0c45d8915df494c23421487ce54d2
Carl-Olivier N'DIaye <carl-olivier.ndiaye@protonmail.com>
2022-10-16 20:15:29 +0200

    SBUS

--- gpiod_test.py
+++ gpiod_test.py
@@ -6,10 +6,13 @@ chip = gpiod.Chip("/dev/gpiochip0", gpiod.Chip.OPEN_BY_NUMBER)
 button = chip.get_line(22)  # pin 7
 button.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_IN)
 
-try:
-  while True:
-    led.set_value(button.get_value())
-finally:
-  led.set_value(0)
-  led.release()
-  button.release()
+while True:
+    if button.event_wait(timedelta(seconds=10)):
+        # event_read() is blocking function.
+        event = button.event_read()
+        if event.event_type == line_event.RISING_EDGE:
+            print("rising: ", event.timestamp)
+        else:
+            print("falling: ", event.timestamp)
+    else:
+        print("timeout(10s)")
