c29f9c33671c87c01841e9ffe2ef38299dde0b53
Carl-Olivier N'DIaye <carl-olivier.ndiaye@protonmail.com>
2022-10-16 19:59:08 +0200

    SBUS

--- gpiod_test.py
+++ gpiod_test.py
@@ -4,18 +4,39 @@ from datetime import timedelta
 import gpiod
 
 
-BUTTON_CHIP = 0
-BUTTON_LINE_OFFSET = 22
+try:
+    if len(sys.argv) > 2:
+        BUTTON_CHIP = sys.argv[1]
+        BUTTON_LINE_OFFSET = int(sys.argv[2])
+        if len(sys.argv) > 3:
+            edge = sys.argv[3]
+            if edge[0] == "r":
+                BUTTON_EDGE = line_request.EVENT_RISING_EDGE
+            elif edge[0] == "f":
+                BUTTON_EDGE = line_request.EVENT_FALLING_EDGE
+            else:
+                BUTTON_EDGE = line_request.EVENT_BOTH_EDGES
+        else:
+            BUTTON_EDGE = line_request.EVENT_BOTH_EDGES
+
+    else:
+        raise Exception()
+# pylint: disable=broad-except
+except Exception:
+    print(
+        """Usage:
+    python3 -m test.button <chip> <line offset> [rising|falling|both]"""
+    )
+    sys.exit()
 
 c = chip(BUTTON_CHIP)
 button = c.get_line(BUTTON_LINE_OFFSET)
 
-config = gpiod.line_request()
+config = line_request()
 config.consumer = "Button"
-config.request_type = gpiod.LINE_REQ_DIR_OUT
+config.request_type = BUTTON_EDGE
 
 button.request(config)
-
 print("event fd: ", button.event_get_fd())
 
 while True:
