4656bf0ddc4081a988edc4b04d809d7fe7c35abb
Carl-Olivier N'DIaye <carl-olivier.ndiaye@protonmail.com>
2022-10-16 20:26:52 +0200

    SBUS

--- gpiod_test.py
+++ gpiod_test.py
@@ -1,17 +1,47 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+#
+# This file is part of libgpiod.
+#
+# Copyright (C) 2017-2018 Bartosz Golaszewski <bartekgola@gmail.com>
+#
+
+'''Simplified reimplementation of the gpiomon tool in Python.'''
+
 import gpiod
+import sys
 
-CONSUMER = "led-demo"
-chip = gpiod.Chip("gpiochip0", gpiod.Chip.OPEN_BY_NUMBER)
-button = chip.get_line(22)  # pin 7
-button.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_IN)
-
-while True:
-    if button.event_wait(10):
-        # event_read() is blocking function.
-        event = button.event_read()
-        if event.event_type == line_event.RISING_EDGE:
-            print("rising: ", event.timestamp)
+if __name__ == '__main__':
+    def print_event(event):
+        if event.type == gpiod.LineEvent.RISING_EDGE:
+            evstr = ' RISING EDGE'
+        elif event.type == gpiod.LineEvent.FALLING_EDGE:
+            evstr = 'FALLING EDGE'
         else:
-            print("falling: ", event.timestamp)
-    else:
-        print("timeout(10s)")
+            raise TypeError('Invalid event type')
+
+        print('event: {} offset: {} timestamp: [{}.{}]'.format(evstr,
+                                                               event.source.offset(),
+                                                               event.sec, event.nsec))
+
+    if len(sys.argv) < 3:
+        raise TypeError('usage: gpiomon.py <gpiochip> <offset1> <offset2> ...')
+
+    with gpiod.Chip(sys.argv[1]) as chip:
+        offsets = []
+        for off in sys.argv[2:]:
+            offsets.append(int(off))
+
+        lines = chip.get_lines(offsets)
+        lines.request(consumer=sys.argv[0], type=gpiod.LINE_REQ_EV_BOTH_EDGES)
+
+        try:
+            while True:
+                ev_lines = lines.event_wait(sec=1)
+                if ev_lines:
+                    for line in ev_lines:
+                        event = line.event_read()
+                        print_event(event)
+        except KeyboardInterrupt:
+            sys.exit(130)
