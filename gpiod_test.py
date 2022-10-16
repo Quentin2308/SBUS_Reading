bbad186e74b1ae3eff6cc7da982ac70897fa5dca
Carl-Olivier N'DIaye <carl-olivier.ndiaye@protonmail.com>
2022-10-16 19:41:41 +0200

    SBUS

--- gpiod_test.py
+++ gpiod_test.py
@@ -1,17 +1,48 @@
 # pylint: disable=missing-docstring
 import sys
 from datetime import timedelta
-import  gpiod
+from .. import chip, line_request, line_event
 
-CONSUMER = "SBUS"
-chip = gpiod.Chip("0", gpiod.Chip.OPEN_BY_NUMBER)
-sbus = chip.get_line(22)  # pin 7
-sbus.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
+try:
+    if len(sys.argv) > 2:
+        BUTTON_CHIP = sys.argv[1]
+        BUTTON_LINE_OFFSET = int(sys.argv[2])
+        if len(sys.argv) > 3:
+            edge = sys.argv[3]
+            if edge[0] == "r":
+                BUTTON_EDGE = line_request.EVENT_RISING_EDGE
+            elif edge[0] == "f":
+                BUTTON_EDGE = line_request.EVENT_FALLING_EDGE
+            else:
+                BUTTON_EDGE = line_request.EVENT_BOTH_EDGES
+        else:
+            BUTTON_EDGE = line_request.EVENT_BOTH_EDGES
+
+    else:
+        raise Exception()
+# pylint: disable=broad-except
+except Exception:
+    print(
+        """Usage:
+    python3 -m test.button <chip> <line offset> [rising|falling|both]"""
+    )
+    sys.exit()
+
+c = chip(BUTTON_CHIP)
+button = c.get_line(BUTTON_LINE_OFFSET)
+
+config = line_request()
+config.consumer = "Button"
+config.request_type = BUTTON_EDGE
+
+button.request(config)
+
+print("event fd: ", button.event_get_fd())
 
 while True:
-    if sbus.event_wait(10):
+    if button.event_wait(timedelta(seconds=10)):
         # event_read() is blocking function.
-        event = sbus.event_read()
+        event = button.event_read()
         if event.event_type == line_event.RISING_EDGE:
             print("rising: ", event.timestamp)
         else:
