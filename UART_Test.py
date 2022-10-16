4011a4a5283c5b58d12574817be246df8632dbd1
Carl-Olivier N'DIaye <carl-olivier.ndiaye@protonmail.com>
2022-10-16 18:22:09 +0200

    SBUS

--- UART_Test.py
+++ UART_Test.py
@@ -56,7 +56,7 @@ serial.flush()
 buf = serial.read(23)
 packet = ba.bitarray(endian='big')
 packet.frombytes(buf)
-
+res = []
 for byte in packet:
     binary_rep = bin(byte)  # convert to binary representation
     res.append(binary_rep[2:])  # remove prefix "0b" and add to list
