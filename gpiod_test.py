44d5c5f3eca9a6dac80d0e8671560dc305ca544c
Carl-Olivier N'DIaye <carl-olivier.ndiaye@protonmail.com>
2022-10-16 19:40:40 +0200

    SBUS

--- gpiod_test.py
+++ gpiod_test.py
@@ -5,13 +5,13 @@ import  gpiod
 
 CONSUMER = "SBUS"
 chip = gpiod.Chip("0", gpiod.Chip.OPEN_BY_NUMBER)
-c = chip.get_line(22)  # pin 7
-c.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
+sbus = chip.get_line(22)  # pin 7
+sbus.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
 
 while True:
-    if button.event_wait(timedelta(seconds=10)):
+    if sbus.event_wait(10):
         # event_read() is blocking function.
-        event = button.event_read()
+        event = sbus.event_read()
         if event.event_type == line_event.RISING_EDGE:
             print("rising: ", event.timestamp)
         else:
