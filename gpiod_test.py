aad8210ba81b6d6916c7561c279ac2a038981777
Carl-Olivier N'DIaye <carl-olivier.ndiaye@protonmail.com>
2022-10-16 20:10:46 +0200

    SBUS

--- gpiod_test.py
+++ gpiod_test.py
@@ -1,51 +1,15 @@
-# pylint: disable=missing-docstring
-import sys
-from datetime import timedelta
+# GPIO used PD30
+
 import gpiod
+import time
 
-print(len(sys.argv))
-try:
-    if len(sys.argv) > 2:
-        BUTTON_CHIP = sys.argv[1]
-        BUTTON_LINE_OFFSET = int(sys.argv[2])
-        if len(sys.argv) > 3:
-            edge = sys.argv[2]
-            if edge[0] == "r":
-                BUTTON_EDGE = line_request.EVENT_RISING_EDGE
-            elif edge[0] == "f":
-                BUTTON_EDGE = line_request.EVENT_FALLING_EDGE
-            else:
-                BUTTON_EDGE = line_request.EVENT_BOTH_EDGES
-        else:
-            BUTTON_EDGE = line_request.EVENT_BOTH_EDGES
-
-    else:
-        raise Exception()
-# pylint: disable=broad-except
-except Exception:
-    print(
-        """Usage:
-    python3 -m test.button <chip> <line offset> [rising|falling|both]"""
-    )
-    sys.exit()
-
-c = chip(BUTTON_CHIP)
-button = c.get_line(BUTTON_LINE_OFFSET)
-
-config = line_request()
-config.consumer = "Button"
-config.request_type = BUTTON_EDGE
-
-button.request(config)
-print("event fd: ", button.event_get_fd())
+chip = gpiod.Chip("/dev/gpiochip0")
+line = gpiod.find_line("22")
+lines = chip.get_lines([line.offset()])
+lines.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_IN)
 
 while True:
-    if button.event_wait(10):
-        # event_read() is blocking function.
-        event = button.event_read()
-        if event.event_type == line_event.RISING_EDGE:
-            print("rising: ", event.timestamp)
-        else:
-            print("falling: ", event.timestamp)
-    else:
-        print("timeout(10s)")
+    print(lines.get_values())
+    time.sleep(1)
+    print(lines.get_values())
+    time.sleep(1)
