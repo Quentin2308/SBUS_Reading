4b37dc475b00692ac745941d000015c108199623
Carl-Olivier N'DIaye <carl-olivier.ndiaye@protonmail.com>
2022-10-16 19:46:28 +0200

    SBUS

--- gpiod_test.py
+++ gpiod_test.py
@@ -1,43 +1,13 @@
 # pylint: disable=missing-docstring
 import sys
 from datetime import timedelta
-from .. import chip, line_request, line_event
+import gpiod
 
-try:
-    if len(sys.argv) > 2:
-        BUTTON_CHIP = sys.argv[1]
-        BUTTON_LINE_OFFSET = int(sys.argv[2])
-        if len(sys.argv) > 3:
-            edge = sys.argv[3]
-            if edge[0] == "r":
-                BUTTON_EDGE = line_request.EVENT_RISING_EDGE
-            elif edge[0] == "f":
-                BUTTON_EDGE = line_request.EVENT_FALLING_EDGE
-            else:
-                BUTTON_EDGE = line_request.EVENT_BOTH_EDGES
-        else:
-            BUTTON_EDGE = line_request.EVENT_BOTH_EDGES
+CONSUMER = "led-demo"
+chip = gpiod.Chip("0", gpiod.Chip.OPEN_BY_NUMBER)
 
-    else:
-        raise Exception()
-# pylint: disable=broad-except
-except Exception:
-    print(
-        """Usage:
-    python3 -m test.button <chip> <line offset> [rising|falling|both]"""
-    )
-    sys.exit()
-
-c = chip(BUTTON_CHIP)
-button = c.get_line(BUTTON_LINE_OFFSET)
-
-config = line_request()
-config.consumer = "Button"
-config.request_type = BUTTON_EDGE
-
-button.request(config)
-
-print("event fd: ", button.event_get_fd())
+button = chip.get_line(13)  # pin 36
+button.request(consumer=CONSUMER, type=gpiod.LINE_REQ_DIR_IN)
 
 while True:
     if button.event_wait(timedelta(seconds=10)):
