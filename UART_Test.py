a1fb02785e6366ccf6e538e75bbcef1068093a15
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-18 13:06:37 +0200

    Update UART_Test.py

--- UART_Test.py
+++ UART_Test.py
@@ -59,7 +59,7 @@ chip = gpiod.Chip("0", gpiod.Chip.OPEN_BY_NUMBER)
 
 sbus = chip.get_line(22)  # pin 07
 while True:
-    if sbus.event_wait([10]):
+    if sbus.event_wait(10):
         # event_read() is blocking function.
         event = button.event_read()
         if event.event_type == line_event.RISING_EDGE:
